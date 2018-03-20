9647017bd06ef57a4b60ef63f6235021f8ba4172
Ivona Malekovic <ivonamalekovic@hotmail.com>
2018-03-20 13:41:55 +0100

    4

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
     return (
       <div className="App">
        <UserInput />
-       <UserOutput />
+       <UserOutput username="Ivona" />
        <UserOutput />
        <UserOutput />
       </div>
--- src/UserOutput/UserOutput.js
+++ src/UserOutput/UserOutput.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./UserOutput.css";
 
-const UserOutput = () => {
+const UserOutput = (props) => {
     return (
         <div className="UserOutputContainer">
             <p className="Paragraph">
-            The lion (Panthera leo) is a species in the family Felidae and a member of the genus Panthera. It is the second largest extant species after the tiger. It exhibits a pronounced sexual dimorphism; males are larger than females with a typical weight range of 150 to 250 kg (331 to 551 lb) for the former and 120 to 182 kg (265 to 401 lb) for the latter. In addition, male lions have a prominent mane, which is perhaps the most recognisable feature of the species. Both sexes have hairy tufts at the end of their tails.
+            {props.username} - The lion (Panthera leo) is a species in the family Felidae and a member of the genus Panthera. It is the second largest extant species after the tiger. It exhibits a pronounced sexual dimorphism; males are larger than females with a typical weight range of 150 to 250 kg (331 to 551 lb) for the former and 120 to 182 kg (265 to 401 lb) for the latter. In addition, male lions have a prominent mane, which is perhaps the most recognisable feature of the species. Both sexes have hairy tufts at the end of their tails.
             </p>
             <p className="Paragraph">
             In the Pleistocene, lions were the most widespread large land mammals and ranged throughout Eurasia, Africa and North America. Today, the lion occurs in fragmented populations in Sub-Saharan Africa and one in western India. It has been listed as Vulnerable on the IUCN Red List since 1996, as populations in African range countries declined by about 43% since the early 1990s. Lion populations are untenable outside designated protected areas. Although the cause of the decline is not fully understood, habitat loss and conflicts with humans are the greatest causes of concern. The Asiatic lion and the West African lion are listed as Endangered and Critically Endangered, respectively.
