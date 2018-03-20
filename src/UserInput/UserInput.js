917ea6fbe6b0fa2ccbd134228a7d59ab1d172666
Ivona Malekovic <ivonamalekovic@hotmail.com>
2018-03-20 13:38:22 +0100

    1 and 2

---
+++ src/UserInput/UserInput.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./UserInput.css";
+
+const UserInput = () => {
+    return (
+        <div className="UserInputContainer">
+          <input className="Input" type="text" />
+        </div>
+      
+    )
+}
+
+export default UserInput;
